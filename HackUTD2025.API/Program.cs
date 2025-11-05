6c9d7bd446d4205f9ee379776c8fb2d2402e7f4f
ebrown2 <ethan_brown@eogresources.com>
2025-11-05 12:23:34 -0600

    Add swagger to prod

--- HackUTD2025.API/Program.cs
+++ HackUTD2025.API/Program.cs
@@ -1,4 +1,5 @@
 using System.Text.Json;
+
 using HackUTD2025.API.Dtos;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,9 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 var data = await ReadHistoricalDataAsync();
 var metadata = await ReadMetadataAsync();
 var dtos = await ReadDataAsync();
-var tickets =  await ReadTicketDataAsync();
+var tickets = await ReadTicketDataAsync();
 
-var graph = new DirectedGraph([..dtos.cauldrons, dtos.enchanted_market], dtos.network.edges);
+var graph = new DirectedGraph([.. dtos.cauldrons, dtos.enchanted_market], dtos.network.edges);
 
 builder.Services.AddSingleton(graph);
 builder.Services.AddSingleton(tickets);
@@ -29,22 +30,20 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI(c => {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "HackUTD2025.API V1");
-    });
-}
+
+app.MapOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI(c => {
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "HackUTD2025.API V1");
+});
+
 
 app.UseRouting();
 
 app.MapControllers();
 
-app.Use((context, next) =>
-{
-    app.Logger.LogInformation("Received request: {Method} {Path}{Query}", context.Request.Method, context.Request.Path,context.Request.QueryString);
+app.Use((context, next) => {
+    app.Logger.LogInformation("Received request: {Method} {Path}{Query}", context.Request.Method, context.Request.Path, context.Request.QueryString);
     return next();
 });
 app.UseResponseCaching();
@@ -55,7 +54,7 @@ async ValueTask<IEnumerable<HistoricalDataDto>> ReadHistoricalDataAsync()
 {
     using StreamReader reader = new("Data/testhistorical_data 2.json");
 
-    var data = await  JsonSerializer.DeserializeAsync<IEnumerable<HistoricalDataDto>?>(reader.BaseStream);
+    var data = await JsonSerializer.DeserializeAsync<IEnumerable<HistoricalDataDto>?>(reader.BaseStream);
 
     return data ?? throw new Exception("Where's our data???");
 }
@@ -64,7 +63,7 @@ async ValueTask<HistoricalDataMetadataDto> ReadMetadataAsync()
 {
     using StreamReader reader = new("Data/testhistorical_metadata.json");
 
-    var data = await  JsonSerializer.DeserializeAsync<HistoricalDataMetadataDto?>(reader.BaseStream);
+    var data = await JsonSerializer.DeserializeAsync<HistoricalDataMetadataDto?>(reader.BaseStream);
 
     return data ?? throw new Exception("Where's our history metadata???");
 }
@@ -73,7 +72,7 @@ async ValueTask<RootDto> ReadDataAsync()
 {
     using StreamReader reader = new("Data/testcauldrons 2.json");
 
-    var data = await  JsonSerializer.DeserializeAsync<RootDto>(reader.BaseStream);
+    var data = await JsonSerializer.DeserializeAsync<RootDto>(reader.BaseStream);
 
     return data ?? throw new Exception("Where's our cauldron data???");
 }
@@ -82,7 +81,7 @@ async ValueTask<TicketsDto> ReadTicketDataAsync()
 {
     using StreamReader reader = new("Data/testtransport_tickets-1 1.json");
 
-    var data = await  JsonSerializer.DeserializeAsync<TicketsDto>(reader.BaseStream);
+    var data = await JsonSerializer.DeserializeAsync<TicketsDto>(reader.BaseStream);
 
     return data ?? throw new Exception("Where's our ticket data???");
 }
